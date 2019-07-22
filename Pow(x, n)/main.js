f046beb81ca68deb25e480072e2af173f4565ac0
Jason_Shu <shuyue524@gmail.com>
2019-07-22 15:35:00 +0800

    modify Pow(x, n)

--- Pow(x, n)/main.js
+++ Pow(x, n)/main.js
@@ -13,7 +13,7 @@ function myPow(x, n) {
     // 先处理边界问题
     if(n === 0) return 1;
     if(n === 1) return x;
-    if(n < 0) return myPow(x, -n);
+    if(n < 0) return 1 / myPow(x, -n);
 
     if(n % 2) {
         return x * myPow(x * x, (n - 1) / 2);
